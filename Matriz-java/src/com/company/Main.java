a57c02f0658fce7d677b0786c0572064b4178d12
Alisson C <alissoncavalcanticma@gmail.com>
2021-06-22 00:04:28 -0300

    Inc Date

--- Matriz-java/src/com/company/Main.java
+++ Matriz-java/src/com/company/Main.java
@@ -1,10 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        int [][] mat = new int [n][n];
 
+        for(int x = 0; x < n; x++){
+            for(int y = 0; y < n; y++){
+                mat[x][y] = sc.nextInt();
+            }
+        }
+
+        sc.close();
     }
 }
