cab74010614c4a7ff73bbf298aa0ada43f45b983
Alisson C <alissoncavalcanticma@gmail.com>
2021-07-03 04:38:56 -0300

    Usando class File

---
+++ Files/src/aplication/Main.java
@@ -0,0 +1,34 @@
+package aplication;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+
+        /*
+        *   Usando class File para ler arquivo.
+        */
+        /*File file = new File("/home/alisson/JavaCourse/Files/morning.txt");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+                System.out.print("Error: " + e.getMessage());
+        }finally{
+            if(sc != null) {
+                sc.close();
+            }
+        }
+        */
+
+    }
+}
