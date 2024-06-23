a6b4a2c396465add9ddac5c505bc5b7196ada569
Alisson Cavalcanti Galvão <alissoncavalcanticma@gmail.com>
2024-06-23 16:49:09 -0300

    handlingDateTimes

---
+++ handlingDateTimes/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+            if(true){
+                System.out.printf("Hello and welcome!");
+            }
+
+            //TIP Testando comments `oli` <shortcut actionId="Alisson"/>
+        }
+}
