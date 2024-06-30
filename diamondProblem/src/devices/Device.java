016f7d231948fd59fe68b0e1b0a48d5400998981
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-06-30 04:35:36 -0300

    Create abstract class Devices

--- diamondProblem/src/devices/Device.java
+++ diamondProblem/src/devices/Device.java
@@ -1,4 +1,20 @@
 package devices;
 
-public class Device {
+public abstract class Device {
+
+    public String serialNumber;
+
+    public Device(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public abstract void processDoc(String doc);
 }
