ada69db1bc371feb2b9708c54c08cc294b4f0c73
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-06-30 05:15:03 -0300

    First commit

---
+++ diamondProblem - solved/src/application/Main.java
@@ -0,0 +1,18 @@
+package application;
+
+import devices.Printer;
+import devices.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Printer p = new Printer("Epson Cx3300");
+        p.processDoc("MyLetter");
+        p.print("MyLetter");
+
+        Scanner s = new Scanner("LG ScanMulti LG2598");
+        s.processDoc("My E-mail");
+        System.out.println("Scan result: " + s.scan());
+
+    }
+}
