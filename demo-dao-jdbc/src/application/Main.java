2d14a2e3f7af902f827589bb0ea7a5a796c0766c
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-08-04 01:44:58 -0300

    create DaoJDBC and DaoFactory class

--- demo-dao-jdbc/src/application/Main.java
+++ demo-dao-jdbc/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Main {
         Department dp = new Department(1, "TI");
 
         Seller seller = new Seller(11, "Alisson", "alisson@als.com.br", new Date(), 5800.00, dp);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(dp);
         System.out.println(seller);
---
+++ demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+
+    @Override
+    public void insert(Seller dp) {
+
+    }
+
+    @Override
+    public void update(Seller dp) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
