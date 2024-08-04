b00c55d672920399d0152a7f8e59198b650d5ed3
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-08-04 01:16:56 -0300

    Create Dao Intefaces

--- demo-dao-jdbc/src/application/Main.java
+++ demo-dao-jdbc/src/application/Main.java
@@ -1,12 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department dp = new Department(1, "TI");
+
+        Seller seller = new Seller(11, "Alisson", "alisson@als.com.br", new Date(), 5800.00, dp);
+
         System.out.println(dp);
+        System.out.println(seller);
 
     }
 }
