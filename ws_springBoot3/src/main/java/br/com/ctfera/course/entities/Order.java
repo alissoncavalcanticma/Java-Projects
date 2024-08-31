37fd1398457a90b82b15eaea30ede2aa9e7f4d18
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-08-31 10:51:55 -0300

    Implementing enum order class with code

--- ws_springBoot3/src/main/java/br/com/ctfera/course/entities/Order.java
+++ ws_springBoot3/src/main/java/br/com/ctfera/course/entities/Order.java
@@ -26,7 +26,7 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
-    private OrderStatus orderStatus;
+    private Integer orderStatus;
 
     @ManyToOne //Anottation de relação muitos para 1
     @JoinColumn(name = "client_id") //definição da coluna/campo de Join
@@ -37,7 +37,7 @@ public class Order implements Serializable {
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
-        this.orderStatus =  orderStatus;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -58,11 +58,13 @@ public class Order implements Serializable {
     }
 
     public OrderStatus getOrderStatus() {
-        return orderStatus;
+        return OrderStatus.valueOf(orderStatus);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
-        this.orderStatus = orderStatus;
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
