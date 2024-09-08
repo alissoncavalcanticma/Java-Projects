04031f613d3610c221320b40fea2527107d3a596
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-09-08 07:01:36 -0300

    Implement update User - fail 2

--- ws_springBoot3/src/main/java/br/com/ctfera/course/resources/UserResource.java
+++ ws_springBoot3/src/main/java/br/com/ctfera/course/resources/UserResource.java
@@ -57,5 +57,6 @@ public class UserResource {
     public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
         obj = userService.update(id, obj);
         return  ResponseEntity.ok().body(obj);
+
     }
 }
